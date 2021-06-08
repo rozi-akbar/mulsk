3bad81eff21ea5c2f9d92d1e78f06a8e7016f9b8
rozi-akbar <akbarrozi8@gmail.com>
2021-06-08 15:55:32 +0700

    about

--- application/modules/About/views/about.php
+++ application/modules/About/views/about.php
@@ -54,18 +54,123 @@
                         <p class="mg__0" style="text-indent: 50px; text-align:justify">
                             Rekomendasi sarung bantal berbahan sutra yang sudah terjamin aman dan berkualitas, MULSK Pillowcase terbuat dari 100% <strong>Mulberry Silk 19 momme</strong>, memiliki sertifikat OEKO-TEX Standard 100 yang artinya bebas dari bahan kimia. Menurut beberapa ulasan, sarung bantal yang terbuat dari kain mulberry silk ini dapat menghaluskan rambut yang rapuh dan kusut, selain itu menjaga kelembaban kulit dan anti-aging. Berikut <strong>benefits</strong> menggunakan <strong>MULKS Pillowcase</strong>:
                         </p>
+                        <br />
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <img src="assets/images/about-us/why-mulsk.jpg" width="100%">
+                            </div>
+                            <div class="col-lg-7">
+                                <p>
+                                    <strong>
+                                        1. Mencegah Kerutan di Wajah
+                                    </strong>
+                                </p>
+                                <p style="text-indent: 50px; text-align:justify">
+                                    Saat kamu menempatkan wajah di atas bantal dalam posisi miring atau tengkurap, maka kerutan-kerutan yang terbentuk di sarung bantal akan ikut tercetak di wajah. Penggunaan sarung bantal berbahan sutra dipercaya akan mengurangi kerutan pada wajah dan mengurangi potensi keriput permanen yang timbul pada kulit wajah.
+                                </p>
+                                <p>
+                                    <strong>
+                                        2. Menjaga Kadar Air dan Kelembaban Pada Kulit Wajah
+                                    </strong>
+                                </p>
+                                <p style="text-indent: 50px; text-align:justify">
+                                    Tekstur kain sutra yang tidak mudah menyerap air dan kelembaban dipercaya membantu menjaga kadar air dan kelembaban pada kulit wajah agar setelah menggunakan skincare tidak berpindah ke sarung bantal.
+                                </p>
+                                <p>
+                                    <strong>
+                                        3. Hypoallergenic
+                                    </strong>
+                                </p>
+                                <p style="text-indent: 50px; text-align:justify">
+                                    Tekstur alami kain sutra menjadikannya bebas dari beberapa penyebab alergi yang biasanya ditemukan pada sarung bantal seperti tungau dan jamur. Memakai sarung bantal dari bahan sutra dipercaya lebih baik untuk Anda pemilik kulit sensitif.
+                                </p>
+                                <p>
+                                    <strong>
+                                        4. Lebih Baik untuk Pemilik Kulit Berjerawat
+                                    </strong>
+                                </p>
+                                <p style="text-indent: 50px; text-align:justify">
+                                    Tekstur permukaan kain sutra yang licin dan halus punya efek lebih baik bagi mereka yang berjerawat. Ini meminimalisasi gesekan yang mungkin mengakibatkankan breakout dan peradangan/penyebaran jerawat.
+                                </p>
+                                <p>
+                                    <strong>
+                                        5. Mencegah Rambut Kusut dan Bercabang
+                                    </strong>
+                                </p>
+                                <p style="text-indent: 50px; text-align:justify">
+                                    Jika kamu memiliki masalah rambut yang kusut setelah bangun tidur juga bisa mencoba memakai sarung bantal sutra.
+                                </p>
+                            </div>
+                        </div>
                     </div>
+                    <!--shipping info-->
+                    <div class="kalles-section nt_section type_shipping kalles-section__type_shipping">
+                        <div class="container bgbl">
+                            <div class="row">
+                                <div class="col-12" style="text-align: center;">
+                                <h3>Mulsk Certifications :</h3>
+                                </div>
+                            </div>
+                            <br/>
+                            <div class="row fl_wrap fl_wrap_md oah use_border_false">
+                                <div class="col-12 col-md-6 col-lg-3 mb__25 bl_1581530479619-0">
+                                    <div class="nt_shipping nt_icon_deafult tl row no-gutters al_center_">
+                                        <div class="col-auto icon medium csi">
+                                            <i class="pegk pe-7s-car"></i>
+                                        </div>
+                                        <div class="col content">
+                                            <h3 class="title cd fs__14 mg__0 mb__5">SGS Certification</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6 col-lg-3 mb__25 bl_1581530479619-1">
+                                    <div class="nt_shipping nt_icon_deafult tl row no-gutters al_center_">
+                                        <div class="col-auto icon medium csi">
+                                            <i class="pegk pe-7s-help2"></i>
+                                        </div>
+                                        <div class="col content">
+                                            <h3 class="title cd fs__14 mg__0 mb__5">SpinControl Idea</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6 col-lg-3 mb__25 bl_1581530479619-2">
+                                    <div class="nt_shipping nt_icon_deafult tl row no-gutters al_center_">
+                                        <div class="col-auto icon medium csi">
+                                            <i class="pegk pe-7s-refresh"></i>
+                                        </div>
+                                        <div class="col content">
+                                            <h3 class="title cd fs__14 mg__0 mb__5">Balai Textile</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6 col-lg-3 mb__25 bl_1581530479619-3">
+                                    <div class="nt_shipping nt_icon_deafult tl row no-gutters al_center_">
+                                        <div class="col-auto icon medium csi">
+                                            <i class="pegk pe-7s-door-lock"></i>
+                                        </div>
+                                        <div class="col content">
+                                            <h3 class="title cd fs__14 mg__0 mb__5">100% PAYMENT SECURE</h3>
+                                            <p class="mg__0">We ensure secure payment with PEV</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end shipping info-->
                 </div>
+                <!-- end tab contents -->
             </div>
-            <!-- end tab contents -->
         </div>
-    </div>
-    <!-- end description and review -->
+        <!-- end description and review -->
 
-</div>
-<script src="assets/js/photoswipe.min.js"></script>
-<script src="assets/js/photoswipe-ui-default.min.js"></script>
-<script src="assets/js/drift.min.js"></script>
-<script src="assets/js/isotope.pkgd.min.js"></script>
-<script src="assets/js/resize-sensor.min.js"></script>
-<script src="assets/js/theia-sticky-sidebar.min.js"></script>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <script src="assets/js/photoswipe.min.js"></script>
+    <script src="assets/js/photoswipe-ui-default.min.js"></script>
+    <script src="assets/js/drift.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/resize-sensor.min.js"></script>
+    <script src="assets/js/theia-sticky-sidebar.min.js"></script>
