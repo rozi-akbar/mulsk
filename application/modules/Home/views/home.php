89356ec3b66f0450881da47efa6a3150f72ba617
rozi-akbar <akbarrozi8@gmail.com>
2021-06-08 09:02:21 +0700

    fix home

--- application/modules/Home/views/home.php
+++ application/modules/Home/views/home.php
@@ -192,6 +192,32 @@
     </div>
     <!-- end our product -->
 
+    <!--full width banner section-->
+    <div class="kalles-section nt_section type_image_text_overlay">
+        <div class="kalles-handmade__full-width-banner nt_full txt_shadow_false se_height_cus_h nt_first">
+            <div class="row equal_nt">
+                <div class="col-12">
+                    <div class="nt_img_txt oh pr middle center">
+                        <div class="js_full_ht4 lazyload item__position bg_rp_norepeat bg_sz_cover center" data-bgset="assets/images/home-hand-made/full-width-banner.png"></div>
+                        <div class="txt_content pa t__0 l__0 b__0 r__0 caption-w-1 flex column shadow_wrap tl pe_none z__100">
+                            <div class="pa pa_txts">
+                                <h3 class="mt__0 mg__0 lh__1">Deal of the day</h3>
+                                <div class="kalles-handmade__full-width-banner__br imtt4_space"></div>
+                                <p class="kalles-handmade__full-width-banner__desc">
+                                    Years of experience brought about by our skilled craftsmen could ensure that every piece<br />
+                                    produced is a work of art. Our focus is always the best quality possible.
+                                </p>
+                                <a class="imtt4_btn button pe_auto round_true btn_icon_false" href="shop.html">Shop Now</a>
+                            </div>
+                        </div>
+                        <a href="shop-hidden-sidebar.html" class="pa t__0 l__0 b__0 r__0"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--end full width banner section-->
+
     <!--blog post-->
     <div class="kalles-section kalles-section_type_featured_blog nt_section type_featured_blog type_carousel">
         <div class="container">
