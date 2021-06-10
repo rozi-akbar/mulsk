82f5396292cf86082d395d95ae55b361e9a2f199
rozi-akbar <akbarrozi8@gmail.com>
2021-06-10 17:05:00 +0700

    banner home

--- application/modules/Home/views/home.php
+++ application/modules/Home/views/home.php
@@ -5,7 +5,7 @@
         <div class="slideshow-wrapper nt_full se_height_cus_h nt_first">
             <div class="fade_flick_1 slideshow row no-gutters equal_nt nt_slider js_carousel prev_next_0 btn_owl_1 dot_owl_2 dot_color_1 btn_vi_2" data-flickity='{ "fade":0,"cellAlign": "center","imagesLoaded": 0,"lazyLoad": 0,"freeScroll": 0,"wrapAround": true,"autoPlay" : 0,"pauseAutoPlayOnHover" : true, "rightToLeft": false, "prevNextButtons": false,"pageDots": true, "contain" : 1,"adaptiveHeight" : 1,"dragThreshold" : 5,"percentPosition": 1 }'>
 
-                <!-- second slide -->
+                <!-- first slide -->
                 <div class="col-12 slideshow__slide">
                     <div class="oh position-relative nt_img_txt bg-black--transparent">
                         <div class="js_full_ht4 img_slider_block kalles-slide-element__pdb-600">
@@ -22,9 +22,9 @@
                         <a href="shop.html" class="pa t__0 l__0 b__0 r__0 pe_none"></a>
                     </div>
                 </div>
-                <!-- end second slide -->
+                <!-- end first slide -->
 
-                <!-- third slide -->
+                <!-- second slide -->
                 <div class="col-12 slideshow__slide">
                     <div class="oh position-relative nt_img_txt bg-black--transparent">
                         <div class="js_full_ht4 img_slider_block kalles-slide-element__pdb-600">
@@ -42,47 +42,7 @@
                         <a href="shop.html" class="pa t__0 l__0 b__0 r__0 pe_none"></a>
                     </div>
                 </div>
-                <!-- end third slide -->
-
-                <!-- third slide -->
-                <div class="col-12 slideshow__slide">
-                    <div class="oh position-relative nt_img_txt bg-black--transparent">
-                        <div class="js_full_ht4 img_slider_block kalles-slide-element__pdb-600">
-                            <div class="bg_rp_norepeat bg_sz_cover lazyload item__position center center img_tran_ef pa l__0 t__0 r__0 b__0" data-bgset="assets/images/homepage-4.jpg"></div>
-                        </div>
-                        <div class="caption-wrap caption-w-1 pe_none z_100 tl_md tl">
-                            <div class="pa_txts caption kalles-caption-layout-01 kalles-caption--midle-left">
-                                <div class="left_right">
-                                    <h4 class="kalles-caption-layout-01__subtitle mg__0 lh__1">SUMMER SALE</h4>
-                                    <h3 class="kalles-caption-layout-01__title mg__0 lh__1">Save up to 70%</h3>
-                                    <a class="kalles-caption-layout-01__button kalles-button--square slt4_btn button pe_auto round_false btn_icon_false" href="shop-filter-options.html">Explore Now</a>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="shop.html" class="pa t__0 l__0 b__0 r__0 pe_none"></a>
-                    </div>
-                </div>
-                <!-- end third slide -->
-
-                <!-- third slide -->
-                <div class="col-12 slideshow__slide">
-                    <div class="oh position-relative nt_img_txt bg-black--transparent">
-                        <div class="js_full_ht4 img_slider_block kalles-slide-element__pdb-600">
-                            <div class="bg_rp_norepeat bg_sz_cover lazyload item__position center center img_tran_ef pa l__0 t__0 r__0 b__0" data-bgset="assets/images/homepage-5.jpg"></div>
-                        </div>
-                        <div class="caption-wrap caption-w-1 pe_none z_100 tl_md tl">
-                            <div class="pa_txts caption kalles-caption-layout-01 kalles-caption--midle-left">
-                                <div class="left_right">
-                                    <h4 class="kalles-caption-layout-01__subtitle mg__0 lh__1">SUMMER SALE</h4>
-                                    <h3 class="kalles-caption-layout-01__title mg__0 lh__1">Save up to 70%</h3>
-                                    <a class="kalles-caption-layout-01__button kalles-button--square slt4_btn button pe_auto round_false btn_icon_false" href="shop-filter-options.html">Explore Now</a>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="shop.html" class="pa t__0 l__0 b__0 r__0 pe_none"></a>
-                    </div>
-                </div>
-                <!-- end third slide -->
+                <!-- end second slide -->
 
                 <!-- third slide -->
                 <div class="col-12 slideshow__slide">
