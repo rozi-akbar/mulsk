ad28443ac617a8be52236f0f896b15e7bb896cca
rozi-akbar <akbarrozi8@gmail.com>
2021-06-14 11:09:51 +0700

    title web

--- application/modules/Layout/views/header.php
+++ application/modules/Layout/views/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="<?= base_url() ?>assets/images/svg/favicon_mulsk.svg">
-    <title>Mulsk Beauty</title>
+    <title>Mulsk Beauty - Berry Good Sleep</title>
     <link rel="preconnect" href="https://fonts.gstatic.com/">
     <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:300,300i,400,400i,500,500i&amp;display=swap" rel="stylesheet"> -->
