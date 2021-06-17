7c31c440455b19404d9211eecbdf2158af9702c5
rozi-akbar <akbarrozi8@gmail.com>
2021-06-17 16:13:42 +0700

    not_found layout

--- application/modules/Not_Found/views/not_found.php
+++ application/modules/Not_Found/views/not_found.php
@@ -5,79 +5,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="shortcut icon" type="image/png" href="<?= base_url() ?>assets/images/svg/favicon_mulsk.svg">
+    <title>Mulsk Beauty - Berry Good Sleep</title>
 </head>
+<style>
+    html,
+    body,
+    #wrapper {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        border: 0;
+    }
+
+    #wrapper td {
+        vertical-align: middle;
+        text-align: center;
+    }
+</style>
 
 <body>
-    <style>
-        /*======================
-    404 page
-=======================*/
-
-
-        .page_404 {
-            padding: 40px 0;
-            background: #fff;
-            font-family: 'Arvo', serif;
-        }
-
-        .page_404 img {
-            width: 100%;
-        }
-
-        .four_zero_four_bg {
-
-            background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
-            height: 400px;
-            background-position: center;
-        }
-
-
-        .four_zero_four_bg h1 {
-            font-size: 80px;
-        }
-
-        .four_zero_four_bg h3 {
-            font-size: 80px;
-        }
-
-        .link_404 {
-            color: #fff !important;
-            padding: 10px 20px;
-            background: #39ac31;
-            margin: 20px 0;
-            display: inline-block;
-        }
-
-        .contant_box_404 {
-            margin-top: -50px;
-        }
-    </style>
-    <section class="page_404">
-	<div class="container">
-		<div class="row">	
-		<div class="col-sm-12 ">
-		<div class="col-sm-10 col-sm-offset-1  text-center">
-		<div class="four_zero_four_bg">
-			<h1 class="text-center ">404</h1>
-		
-		
-		</div>
-		
-		<div class="contant_box_404">
-		<h3 class="h2">
-		Look like you're lost
-		</h3>
-		
-		<p>the page you are looking for not avaible!</p>
-		
-		<a href="" class="link_404">Go to Home</a>
-	</div>
-		</div>
-		</div>
-		</div>
-	</div>
-</section>
+    <table id="wrapper">
+        <tr>
+            <td>
+                <img src="<?= base_url() ?>/assets/images/404.png" alt="">
+                <br>
+                <a href="<?= site_url('Home') ?>">Back to Home</a>
+            </td>
+        </tr>
+    </table>
 </body>
 
 </html>
