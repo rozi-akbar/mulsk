ade22252da46c2325d2989fc5a9d5d500374b184
rozi-akbar <akbarrozi8@gmail.com>
2021-06-20 06:31:35 +0700

    change layout not_found

--- application/modules/Not_Found/views/not_found.php
+++ application/modules/Not_Found/views/not_found.php
@@ -1,11 +1,21 @@
 <div id="nt_content">
-    <div class="empty_cart_page tc">
+    <div class="empty_cart_page tc bg-img">
         <div class="container">
             <section class="error-404 not-found">
-                <h1>404</h1>
-                <h3 class="page-title">Sorry! Page you are looking can’t be found.</h3>
-                <p>Go back to the <a href="<?= base_url() ?>" rel="home">homepage</a></p>
+                <div>
+                    <h1>404</h1>
+                    <h3 class="page-title">Sorry! Page you are looking can’t be found.</h3>
+                    <p>Go back to the <a href="<?= base_url() ?>" rel="home">homepage</a></p>
+                </div>
             </section>
         </div>
     </div>
 </div>
+<style>
+    .bg-img {
+        background-image: url('<?= base_url() ?>assets/images/bg-404-mulsk.jpg');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: contain;
+    }
+</style>
