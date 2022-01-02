885bd067e9069d3d908e5c6fe276ca7583f784e8
vickyagassi <vicky@msglowid.com>
2022-01-02 13:03:59 +0700

    change home website dengan data dari cms

--- application/modules/Home/controllers/Home.php
+++ application/modules/Home/controllers/Home.php
@@ -12,8 +12,11 @@ class Home extends CI_Controller
 
     public function index()
     {
-        $data['banner_home']    =   $this->model->Code('SELECT * FROM banner_home WHERE is_deleted = 0 ORDER BY banner_id ASC');
-        
+        $data['banner_home']    = $this->model->Code('SELECT * FROM banner_home WHERE is_deleted = 0 ORDER BY banner_id ASC');
+        $data['vBlog']          = $this->model->View('v_blog_posted', 'id');
+        $data['dataMaster']     = $this->model->View('v_m_product', 'id');
+        $data['vColor']      = $this->model->View('v_color_image_selector', 'id');
+
         $this->load->view('Layout/header');
         $this->load->view('home', $data);
         $this->load->view('Layout/footer');
