31c139e278af21e8bc39be7452ef97a5c36ca15a
vickyagassi <vicky@msglowid.com>
2022-01-03 14:48:43 +0700

    handler update image gallery

--- application/modules/MasterProduct/views/editGallery.php
+++ application/modules/MasterProduct/views/editGallery.php
@@ -63,8 +63,8 @@
                                     </div>
                                 </div>
                                 <div class="col-12">
+                                    <div id="sending"> </div>
                                     <button type="submit" id="update" title="Save Change" id="submit" class="btn btn-primary"> Save Change </button>
-                                    <span id="sending"> </span>
                                     <a href="<?= base_url() ?>/MasterProduct/M_Product_Act/DeleteGallery/<?= $id_master ?>/<?= $data_gallery['id'] ?>">
                                         <button title="Delete" class="btn btn-danger btn-icon">
                                             <i class="fa fa-trash"></i>
