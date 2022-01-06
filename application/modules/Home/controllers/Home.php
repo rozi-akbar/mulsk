7aec6e85297e8dd61ba907475ef722282f605056
vickyagassi <vicky@msglowid.com>
2022-01-06 14:17:54 +0700

    bug image selector home

--- application/modules/Home/controllers/Home.php
+++ application/modules/Home/controllers/Home.php
@@ -15,7 +15,7 @@ class Home extends CI_Controller
         $data['banner_home']    = $this->model->Code('SELECT * FROM banner_home WHERE is_deleted = 0 ORDER BY banner_id ASC');
         $data['vBlog']          = $this->model->Code("SELECT * FROM v_blog_posted ORDER BY id DESC LIMIT 6");
         $data['dataMaster']     = $this->model->View('v_m_product', 'id');
-        $data['vColor']      = $this->model->View('v_color_image_selector_backup', 'id');
+        $data['vColor']      = $this->model->View('v_color_image_selector', 'id');
 
         $this->load->view('Layout/header');
         $this->load->view('home', $data);
