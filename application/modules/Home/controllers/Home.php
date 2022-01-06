1e267618037c1a13af0649389fd1ab371eca0d82
vickyagassi <vicky@msglowid.com>
2022-01-06 15:46:39 +0700

    header home show desc

--- application/modules/Home/controllers/Home.php
+++ application/modules/Home/controllers/Home.php
@@ -12,7 +12,7 @@ class Home extends CI_Controller
 
     public function index()
     {
-        $data['banner_home']    = $this->model->Code('SELECT * FROM banner_home WHERE is_deleted = 0 ORDER BY banner_id ASC');
+        $data['banner_home']    = $this->model->Code('SELECT * FROM banner_home WHERE is_deleted = 0 ORDER BY banner_id DESC');
         $data['vBlog']          = $this->model->Code("SELECT * FROM v_blog_posted ORDER BY id DESC LIMIT 6");
         $data['dataMaster']     = $this->model->View('v_m_product', 'id');
         $data['vColor']         = $this->model->View('v_color_image_selector', 'id');
