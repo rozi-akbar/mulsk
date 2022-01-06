a34940b63aea29dedb0f92beba822342527e0c7a
vickyagassi <vicky@msglowid.com>
2022-01-06 14:26:08 +0700

    bug image selector home

--- application/modules/Home/views/home.php
+++ application/modules/Home/views/home.php
@@ -131,46 +131,50 @@
                 <?php
                 foreach ($dataMaster as $vaProduct) {
                 ?>
-                    <article class="post_nt_loop post_1 col-lg-4 col-md-4 col-12 pr_animated done mb__40">
-                        <div class="product-image position-relative oh lazyload">
-                            <span class="tc nt_labels pa pe_none cw"></span>
-                            <a class="d-block" href="<?= site_url('Products/detailProduct/' . $vaProduct['id']) ?>">
-                                <div class="pr_lazy_img main-img nt_img_ratio nt_bg_lz lazyload padding-top__127_571" data-bgset="<?= base_url($vaProduct['image']) ?>"></div>
-                            </a>
-                            <div class="hover_img pa pe_none t__0 l__0 r__0 b__0 op__0">
-                                <div class="pr_lazy_img back-img pa nt_bg_lz lazyload padding-top__127_571" data-bgset="<?= base_url($vaProduct['image']) ?>"></div>
-                            </div>
-                            <div class="hover_button op__0 tc pa flex column ts__03">
-                                <a class="pr nt_add_qv js_add_qv js_giftset cd br__40 pl__25 pr__25 bgw tc dib ttip_nt tooltip_top_left" href="<?= site_url('Products/detailProduct/' . $vaProduct['id']) ?>">
-                                    <span class="tt_txt">Click to View</span>
-                                    <i class="iccl iccl-eye"></i>
-                                    <span>Click to View</span>
+                    <div class="col-lg-3 col-md-3 col-6 pr_animated done mt__30 pr_grid_item product nt_pr desgin__1">
+                        <div class="product-inner pr">
+                            <div class="product-image position-relative oh lazyload">
+
+                                <a class="d-block" href="<?= base_url() ?>Products/detailProduct/<?= $vaProduct['id'] ?>">
+                                    <div class="pr_lazy_img main-img nt_img_ratio nt_bg_lz lazyload padding-top__127_571" data-bgset="<?= base_url() . $vaProduct['image'] ?>"></div>
                                 </a>
+                                <div class="hover_img pa pe_none t__0 l__0 r__0 b__0 op__0">
+                                    <div class="pr_lazy_img back-img pa nt_bg_lz lazyload padding-top__127_571" data-bgset="<?= base_url() . $vaProduct['image'] ?>"></div>
+                                </div>
+                                <div class="hover_button op__0 tc pa flex column ts__03">
+                                    <a class="pr nt_add_qv js_add_qv js_pillowcase cd br__40 pl__25 pr__25 bgw tc dib ttip_nt tooltip_top_left" href="<?= base_url() ?>Products/detailProduct/<?= $vaProduct['id'] ?>">
+                                        <span class="tt_txt">Click to View</span>
+                                        <i class="iccl iccl-eye"></i>
+                                        <span>Click to View</span>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="product-info mt__15">
-                            <h3 class="product-title position-relative fs__14 mg__0 fwm">
-                                <a class="cd chp" href="<?= site_url('Products/detailProduct/' . $vaProduct['id']) ?>"><b><?= $vaProduct['nama_product'] ?></b></a>
-                            </h3>
-                            <span class="price dib mb__5"><?= "Rp " . number_format($vaProduct['price'], 2, ',', '.'); ?></span>
-                            <br />
-                            <div class="swatch__list_js swatch__list lh__1 nt_swatches_on_grid">
-                                <?php
-                                foreach ($vColor as $keyColor => $vaDataSelector) {
-                                    if ($vaProduct['product_id'] == $vaDataSelector['m_product_id']) {
-                                ?>
-                                        <span data-bgset="<?= base_url() . $vaDataSelector['url_image'] ?>" class="lazyload nt_swatch_on_bg swatch__list--item position-relative ttip_nt tooltip_top_right">
-                                            <span class="tt_txt"><?= $vaDataSelector['color_name'] ?></span>
-                                            <span class="swatch__value" style="background-color: <?= $vaDataSelector['color'] ?>;"></span>
-                                        </span>
-                                <?php
-                                    } else {
+                            <div class="product-info mt__15">
+                                <h3 class="product-title position-relative fs__14 mg__0 fwm">
+                                    <a class="cd chp" href="<?= base_url() ?>Products/detailProduct/<?= $vaProduct['id'] ?>"><b><?= $vaProduct['nama_product'] ?></b></a>
+                                </h3>
+                                <span class="price dib mb__5">
+                                    <?= "Rp " . number_format($vaProduct['price'], 2, ',', '.'); ?>
+                                </span>
+                                <br />
+                                <div class="swatch__list_js swatch__list lh__1 nt_swatches_on_grid">
+                                    <?php
+                                    foreach ($vColor as $keyColor => $vaDataSelector) {
+                                        if ($vaProduct['product_id'] == $vaDataSelector['m_product_id']) {
+                                    ?>
+                                            <span data-bgset="<?= base_url() . $vaDataSelector['url_image'] ?>" class="lazyload nt_swatch_on_bg swatch__list--item position-relative ttip_nt tooltip_top_right">
+                                                <span class="tt_txt"><?= $vaDataSelector['color_name'] ?></span>
+                                                <span class="swatch__value" style="background-color: <?= $vaDataSelector['color'] ?>;"></span>
+                                            </span>
+                                    <?php
+                                        } else {
+                                        }
                                     }
-                                }
-                                ?>
+                                    ?>
+                                </div>
                             </div>
                         </div>
-                    </article>
+                    </div>
                 <?php
                 }
                 ?>
